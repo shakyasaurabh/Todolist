335ea6b1c89c70fc5c34957978b6122194e4b953
shakyasaurabh <saurabhshakya078@gmail.com>
2023-09-05 01:05:21 +0530

    Optimised code and added enter function and auto focus everytime on input

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+// Welcome to my timepass project , do follow me on github 
+// #### https://github.com/shakyasaurabh #######
+// for more simple projects like this ;) <3
+
+// Variables
+
 const inputbox = document.getElementById("inputbox");
 const listitems = document.getElementById("listitems");
 const items = document.querySelectorAll("li");
-const bt = document.querySelector("button");
+const button = document.querySelector("button");
 
-
+// Adding items in list function
 
 function additem(){
+
+// checking if filed is empty, if not add a item
+
     if(inputbox.value == ""){
         alert('Please enter a task');
     }else{
@@ -20,13 +29,15 @@ function additem(){
     console.log("added");
 }; 
 
-bt.addEventListener('keydown', function(event){
-    console.log("key pressed");
-    if (event.keyCode === 13 || event.key === `Enter`){
-        additem();
-        // btn.click();
+// Enter to call additem function 
+
+document.body.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      additem();
     }
 });
+
+// click feature to check and delete item
 
 listitems.addEventListener('click', function(e){
     if(e.target.tagName === "LI"){
@@ -36,13 +47,20 @@ listitems.addEventListener('click', function(e){
          e.target.parentElement.remove();
          saveData();
     }
-},false);
+});
+
+// functions to save data in browser`s memory
 
 function saveData(){
     localStorage.setItem("data", listitems.innerHTML);
 }
 
+// retrive data from browser`s memory
+
 function getData(){
     listitems.innerHTML = localStorage.getItem("data");
 }
+
+// Calling retrive data automatically so it can show
+// previously added items
 getData();
