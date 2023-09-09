d717cc9c6a92ec55e7e25f591f9ecb08e6f7bb79
shakyasaurabh <saurabhshakya078@gmail.com>
2023-09-10 00:29:31 +0530

    PWA

---
+++ sw.js
@@ -0,0 +1,18 @@
+// This is a basic service worker that caches the HTML file.
+const CACHE_NAME = 'my-pwa-cache';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(['/', '/index.html']);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
