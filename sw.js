c954884a72d7fc2bec1ba9f48a5ab393b19758f8
shakyasaurabh <saurabhshakya078@gmail.com>
2023-09-10 00:38:04 +0530

    PWA try-2

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-// This is a basic service worker that caches the HTML file.
-const CACHE_NAME = 'my-pwa-cache';
+const CACHE_VERSION = 'v1'; // Update this when you make changes to your service worker.
+const CACHE_NAME = `my-pwa-cache-${CACHE_VERSION}`;
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(['/', '/index.html']);
+            return cache.addAll(['/index.html']);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                })
+            );
         })
     );
 });
@@ -12,7 +26,29 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+            // Cache hit - return the cached response.
+            if (response) {
+                return response;
+            }
+
+            // Clone the request since it's a stream and can be consumed only once.
+            const fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then((response) => {
+                // Check if we received a valid response.
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+
+                // Clone the response because it's a stream and can be consumed only once.
+                const responseToCache = response.clone();
+
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            });
         })
     );
 });
